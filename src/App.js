5d4472e7566a9bce1eb17cab075fbaf20118706c
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-04-12 14:31:24 +0300

    little clean up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react"
+import React, { useState, useEffect} from "react"
 import { Container, AppBar, Typography, Grow, Grid, InputBase, Button, Paper } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { getBiases, postBias } from "./api/index.js"
@@ -24,6 +24,13 @@ function App() {
   }
 
   const [bias, setBias] = useState({ biasText : ""})
+
+  // TODO: move this get request to a separate component 
+  /*useEffect(() => {
+    const biases = getBiases()
+    console.log(biases)
+    setBias({...bias, biasList: biases})
+  }, [bias])*/
 
   // TODO: clear the form after submit properly
   const handleSubmit = (e) => {
@@ -55,28 +62,15 @@ function App() {
       </form>
         <Grid className={classes.test}>
           <Grid item size="xs">
-            <Typography fullWithh variant="h3" fontWeight="fontWeightLight" align="center">{bias.biasText}</Typography>
+            <Typography variant="h3" align="center">{bias.biasText}</Typography>
           </Grid>
         </Grid>
         <Container>
+          <br/>
           <p>
             People have constant inner dialogs with oneself which often stay unnoticed. Logical distortions in such dialogs may lead to unwanted negative emotional states. You can try to indetify dialogs and biases in own thinking. To reduce stress try to come up with more rational variants. Here is a list of ten common biases:
           </p>
           <br/>
-          <p>
-            "ALL-OR-NOTHING THINKING": "ALL-OR-NOTHING THINKING",
-            "OVERGENERALIZATION": "OVERGENERALIZATION",
-            "MENTAL FILTER": "MENTAL FILTER",
-            "DISQUALIFYING THE POSITIVE": "DISQUALIFYING THE POSITIVE",
-            "JUMPING TO CONCLUSIONS": "JUMPING TO CONCLUSIONS",
-            "Mind reading": "Mind reading",
-            "The Fortune Teller Error": "The Fortune Teller Error",
-            "MAGNIFICATION (CATASTROPHIZING) OR MINIMIZATION":"MAGNIFICATION (CATASTROPHIZING) OR MINIMIZATION",
-            "EMOTIONAL REASONING": "EMOTIONAL REASONING",
-            "SHOULD STATEMENTS": "SHOULD STATEMENTS",
-            "LABELING AND MISLABELING": "LABELING AND MISLABELING:",
-            "PERSONALIZATION": "PERSONALIZATION"
-          </p>
         </Container>
     </Container>
     </>
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
-const url = "http://localhost:5000" // "https://clearthinking.herokuapp.com"
+const url = "http://localhost:5000/bias" // "https://clearthinking.herokuapp.com"
 
-export const getBiases = () => axios.get(url)
+export const getBiases = async () => await axios.get(url).then(response => {
+     return response.data
+}, error => {
+    console.log(error)
+})
 export const postBias = (bias) => axios.post(url, bias).then(response => { 
-    console.log(response)
+    console.log("post response: ", response)
 }, error => {
     console.log(error)
 })
